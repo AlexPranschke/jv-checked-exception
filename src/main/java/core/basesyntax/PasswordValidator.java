80c00b396052287bb1ebce7b9aa92128ce740f4d
AlexPranschke <alexpranschke@gmail.com>
2026-03-15 13:21:29 +0100

    first try

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
-public class PasswordValidator {
-    public void validate(String password, String repeatPassword) {
+public class PasswordValidator  {
+    public void validate(String password, String repeatPassword) throws PasswordValidationException   {
         //write your code here
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+        if(!password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+         if (password.length() < 8) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,6 +3,13 @@ package core.basesyntax;
 public class UserService {
     public void registerUser(User user) {
         //write your code here
+        PasswordValidator passwordValidator = new PasswordValidator();
+        try {
+            passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void saveUser(User user) {
